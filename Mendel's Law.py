03c867a474cf8fd042509984c3d7a5f23936506e
Miranda Walsh <walsh_miranda@yahoo.ca>
2020-11-09 13:26:46 -0500

    Update Mendel's Law.py

--- Mendel's Law.py
+++ Mendel's Law.py
@@ -4,6 +4,11 @@ Created on Mon Nov  9 02:02:24 2020
 
 @author: walsh
 """
+#Problem
+#Given: Three positive integers k, m, and n, representing a population containing k+m+n organisms: k individuals are homozygous dominant for a factor, m are heterozygous, and n are homozygous recessive.
+
+#Return: The probability that two randomly selected mating organisms will produce an individual possessing a dominant allele (and thus displaying the dominant phenotype). Assume that any two organisms can mate.
+
 
 k = 2
 m = 2
@@ -13,3 +18,5 @@ population = k + m + n
 
 prob = (k/population) + (m/population*((k/(population-1)) + (0.75*(m-1)/(population-1) + (0.5*(n/(population-1))))) + (n/population*((k/(population-1)) + (0.5*(m/(population-1))))))                      
 print(prob)
+
+#Expected Output: 0.78333
