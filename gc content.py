297c1292645dc90024bfdba0c3d7765d8f170383
Miranda Walsh <walsh_miranda@yahoo.ca>
2020-11-09 15:15:06 -0500

    Update gc content.py

--- gc content.py
+++ gc content.py
@@ -4,6 +4,11 @@ Created on Mon Nov  9 14:49:19 2020
 
 @author: walsh
 """
+#Problem
+
+#Given: At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
+
+#Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. Rosalind allows for a default error of 0.001 in all decimal answers unless otherwise stated; please see the note on absolute error below.
 
 from Bio.SeqUtils import GC
 
@@ -24,3 +29,8 @@ list = [a, b, c]
 x = (max(list))
 print(x)
 print(seqDict.get(x))
+
+
+#Expected Output: 
+#Rosalind_0808
+#60.919540
