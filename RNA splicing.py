6346e7500189c16e12f12aca7b71c8379ecec0b5
Miranda Walsh <walsh_miranda@yahoo.ca>
2020-11-12 20:10:20 -0500

    Update RNA splicing.py

--- RNA splicing.py
+++ RNA splicing.py
@@ -4,6 +4,10 @@ Created on Wed Nov 11 20:05:56 2020
 
 @author: walsh
 """
+#Problem
+#Given: A DNA string s (of length at most 1 kbp) and a collection of substrings of s acting as introns. All strings are given in FASTA format.
+
+#Return: A protein string resulting from transcribing and translating the exons of s. (Note: Only one solution will exist for the dataset provided.)
 
 from Bio.Seq import Seq
 
@@ -16,3 +20,5 @@ splice_DNAstrand2 = Seq(splice_DNAstrand.replace("ATCGGTCGAGCGTGT", ""))
 
 Protein = splice_DNAstrand2.translate()
 print(Protein)
+
+#Expected Output:  MVYIADKQHVASREAYGHMFKVCA
